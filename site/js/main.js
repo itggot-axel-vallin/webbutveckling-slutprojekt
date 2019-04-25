2aa9aae7116a3cbda7396da7b1eef170713be413
itggot-axel-vallin <axel.vallin@elev.ga.ntig.se>
2019-04-25 15:39:04 +0200

    gjorde klart index och nav menyn

---
+++ site/js/main.js
@@ -0,0 +1,13 @@
+function showHamburger(event){
+    let menu = document.querySelector("nav")
+    menu.classList.toggle("navhidden")
+}
+
+function showCategories(event){
+    let menu = document.querySelector(".navearth")
+    menu.classList.toggle("earthhide")
+    let cat = document.querySelector(".categories")
+    cat.classList.toggle("showcat")
+    // let elem = document.querySelector("")
+}
+
